ba8543d12b45787e15b141b27a8198fb6504efd8
GomoxK <yatozamora1999@gmail.com>
2020-10-22 21:35:47 -0600

    Creacion e implementacion del archivo de rutas para el servidor

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 // Importar modulos requeridos para el servidor
 const express = require("express");
 const exphbs = require("express-handlebars");
-//const router = require("./routes/index");
+const router = require("./routes/index");
 
 //Habilitar el archivo de variables de entorno
-//require("dotenv").config({ path: ".env" });
+require("dotenv").config({ path: ".env" });
 
 // Crear un servidor utilizando express
 const app = express();
 
 //Implementar router
-app.use("/",(req,res,next)=> {
-res.send("Bienvenido a e-clothes");
+app.use("/", router());
 
-});
-
-
-
-
-
-
-app.listen(5000);
+app.listen(process.env.PORT);
