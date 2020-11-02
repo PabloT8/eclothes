8d27763a0f8a61123cd6702078e3ad30dc2137da
PabloT <tavarespablo7@gmail.com>
2020-11-02 16:17:52 -0600

    Configuracion de restablecimiento de contraseña

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 // Importar los modulos requeridos
 const passport = require("passport");
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 const Usuario = mongoose.model("Usuarios");
+const enviarCorreo = require("../handlers/email");
 
 // Se encarga de autenticar el usuario y de redireccionarlo
 exports.autenticarUsuario = passport.authenticate("local", {
@@ -25,9 +27,81 @@ exports.cerrarSesion = (req, res, next) => {
 
 // Mostrar el formulario de restablecer la contraseña
 exports.formularioRestablecerPassword = (req, res, next) => {
-  res.render("restablecerPassword", {layout: "auth"});
+  res.render("restablecerPassword", {
+    layout: "auth",
+    typePage: "register-page",
+    signButtonValue: "/iniciar-sesion",
+    signButtonText: "Iniciar sesion",
+    year: new Date().getFullYear(),
+    messages: req.flash(),  
+  });
+  
 }
 
 // Enviamos un token de autenticación al usuario para cambiar su
 // contraseña. El token se envía al correo del usuario.
-exports.enviarToken = async (req, res, next) => {};
+exports.enviarToken = async (req, res, next) => {
+  // Obtener la direccion de correo electronico
+  const { email } = req.body;
+  const messages = { messages: [] }
+
+  // Buscar el usuario
+  try {
+    const usuario =  await Usuario.findOne({ email });
+
+
+  //Si el usuario no existe
+  if (!usuario) {
+    messages.messages.push({messages: "No existe una cuenta registrada con este correo electronico",
+    alertType: "danger"
+  });
+
+  req.flash(messages);
+
+  res.redirect("/olvide-password");
+  }
+
+  //El usuario existe, generar un token y una fecha de vencimiento
+  usuario.token = crypto.randomBytes(20).toString("hex");
+  usuario.expira = Date.now() + 3600000;
+
+  // Guardar los cambios
+  await usuario.save();
+
+  // Generar la URL de restablecer contrasena
+  const resetUrl = 'http://${req.headers.host}/olvide-password/${usuario.token}';
+
+
+  try {
+    //Enviar la notificacion al correo electronico del usuario
+  const sendMail = await enviarCorreo.enviarCorreo({
+    to: usuario.email,
+    subject: "Restablecer tu contrasena en Eclothes",
+    template: "resetPassword",
+    resetUrl,
+  });
+
+  } catch (error) {
+    console.log(error);
+  }
+  
+
+  // Redireccionar al incio de sesion
+  messages.messages.push({
+    messages: "Verifica tu bandeja de entrada y sigue las instrucciones",
+    alertType: "success",
+  });
+
+  req.flash(messages);
+
+  res.redirect("/iniciar-sesion");
+  } catch (error){
+    messages.messages.push({messages: "Ocurrio un error al momento de comunicarse con el servidor, favor intentar nuevamente",
+    alertType: "danger",
+  });
+
+  req.flash(messages);
+  }
+
+ 
+};
