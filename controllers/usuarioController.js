0aa80a84f331804d3d3f131c4d13f4f254c20d90
GomoxK <yatozamora1999@gmail.com>
2020-11-01 18:42:38 -0600

    Funcion para enviar los mensajes de formulario de registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -3,60 +3,74 @@ const mongoose = require("mongoose");
 const Usuario = mongoose.model("Usuarios");
 const { validationResult } = require("express-validator");
 
+const year = new Date().getFullYear();
+
 // Cargar el formulario de la creación de una cuenta de usuario
 exports.formularioCrearCuenta = (req, res, next) => {
-  res.render("registrarse", { layout: "auth" });
+  res.render("registrarse", {
+    layout: "auth",
+    typePage: "register-page",
+    signButtonValue: "/iniciar-sesion",
+    signButtonText: "Iniciar sesión",
+    year,
+  });
 };
 
 // Procesar el formulario de creación de cuenta
 exports.crearCuenta = async (req, res, next) => {
-
-  //verificar que no existen errores de validacion 
+  // Verificar que no existan errores de validación
   const errores = validationResult(req);
-  const erroresArray = [];
-
-  //console.log(errores);
-  //Si hay errores
-  if (!errores.isEmpty){
-    //Utilizar funcion map para navegar dentro de un arreglo
-    errores.array().map(error => messages.push({ message: error.msg, alertType: "danger" }));
-    //Agregar errores a mensajes flash
-    req.flash("error", erroresArray);
-    res.render("registrarse", {
-      layout: "auth", 
-      messages: errores,
-    });
-  } else{
-    // Obtener las variables desde el cuerpo de la petición
+  const messages = [];
+  // Obtener las variables desde el cuerpo de la petición
   const { nombre, email, password } = req.body;
 
-  // Intentar almacenar los datos del usuario
-  try {
-    // Crear el usuario
-    // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Promise
-    // https://developer.mozilla.org/es/docs/Learn/JavaScript/Asynchronous/Async_await
-    await Usuario.create({
-      email,
-      password,
-      nombre,
-    });
+  // Si hay errores
+  if (!errores.isEmpty()) {
+    // Utilizar la función map para navegar dentro de un arreglo
+    errores
+      .array()
+      .map((error) =>
+        messages.push({ message: error.msg, alertType: "danger" })
+      );
 
-    // Mostrar un mensaje
-    const mensaje = [];
-    mensaje.push({ message: "Usuario creado satisfactoriamente", alertType: "success"});
-    req.flash("error", mensaje);
-    res.redirect("/iniciar-sesion");
-  } catch (error) {
-    console.log(error);
-  }
-  }
+    // Agregar los errores a nuestro mensajes flash
+    req.flash("messages", messages);
 
+    res.redirect("/crear-cuenta");
+  } else {
+    // Intentar almacenar los datos del usuario
+    try {
+      // Crear el usuario
+      // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Promise
+      // https://developer.mozilla.org/es/docs/Learn/JavaScript/Asynchronous/Async_await
+      await Usuario.create({
+        email,
+        password,
+        nombre,
+      });
+
+      // Mostrar un mensaje
+      messages.messages.push({
+        message: "!Usuario creado satisfactoriamente!",
+        alertType: "success",
+      });
+      req.flash("error", messages);
+
+      res.redirect("/iniciar-sesion");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
 
-// Cargar el formulario de inicio de sesion
+// Cargar el formulario de inicio de sesión
 exports.formularioIniciarSesion = (req, res, next) => {
-  res.render("iniciarSesion", { 
+  res.render("iniciarSesion", {
     layout: "auth",
+    typePage: "login-page",
+    signButtonValue: "/crear-cuenta",
+    signButtonText: "Regístrate",
+    year,
     messages: req.flash(),
-});
-}
+  });
+};
