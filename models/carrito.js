d309c4d55ace806255f597a1c28ed53ed170bd85
Luis Escobar <lescobar.099@gmail.com>
2020-12-07 17:50:38 -0600

    funcionalidad de carrito con vistas condicionadas si el usuraio esta logeado + pago con paypal y su redireccionamiento

---
+++ models/carrito.js
@@ -0,0 +1,24 @@
+module.exports = function Carrito(viejoCarrito) {
+    this.items = viejoCarrito.items || {};
+    this.cantidadTotal = viejoCarrito.cantidadTotal || 0;
+    this.precioTotal = viejoCarrito.precioTotal || 0;
+
+    this.add = function(item, id){
+        var itemAlmacenado = this.items[id];
+        if (!itemAlmacenado) {
+            itemAlmacenado = this.items[id] = {item: item, qty: 0, precio: 0};
+        }
+        itemAlmacenado.qty++;
+        itemAlmacenado.precio = itemAlmacenado.item.precio * itemAlmacenado.qty;
+        this.cantidadTotal++;
+        this.precioTotal += itemAlmacenado.item.precio;
+    };
+
+    this.generarArray = function() {
+        var arr = [];
+        for (var id in this.items){
+            arr.push(this.items[id]);
+        }
+        return arr;
+    };
+};
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,32 @@
 // Importar los módulos requeridos
 const express = require("express");
+var Producto = require("../models/Producto");
+var Carrito = require("../models/carrito");
+var Orden = require("../models/orden");
 const usuarioController = require("../controllers/usuarioController");
 const authController = require("../controllers/authController");
 const productoController = require("../controllers/productoController");
 const homeController = require("../controllers/homeController");
 const { check } = require("express-validator");
+const passport = require("passport");
+const orden = require("../models/orden");
+//var csrf = require("csurf");
+
+
 
 // Configura y mantiene todos los endpoints en el servidor
 const router = express.Router();
 
 module.exports = () => {
+  //var csrfProtection = csrf();
+//router.use(csrfProtection);
   // Rutas disponibles
   router.get("/", homeController.mostrarProductos);
 
   // Rutas para usuario
-  router.get("/crear-cuenta", usuarioController.formularioCrearCuenta);
-
-  router.post(
-    "/crear-cuenta",
-    [
-      // Realizar una verificación de los atributos del formulario
-      // https://express-validator.github.io/docs/index.html
-      check("nombre", "Debes ingresar tu nombre completo.")
-        .not()
-        .isEmpty()
-        .escape(),
-      check("email", "Debes ingresar un correo electrónico.").not().isEmpty(),
-      check("email", "El correo electrónico ingresado no es válido.")
-        .isEmail()
-        .normalizeEmail(),
-      check("password", "Debes ingresar una contraseña").not().isEmpty(),
-    ],
-    usuarioController.crearCuenta
-  );
-
-  router.get("/iniciar-sesion", usuarioController.formularioIniciarSesion);
-
-  router.post("/iniciar-sesion", authController.autenticarUsuario);
-
-  router.get("/olvide-password", authController.formularioRestablecerPassword);
-
-  router.post("/olvide-password", authController.enviarToken);
-
-  router.get("/olvide-password/:token", authController.formularioNuevoPassword);
-
-  router.post("/olvide-password/:token", authController.almacenarNuevaPassword);
-
-  // Rutas de administración
-  router.get("/administrar", (req, res, next) => {
-    res.send("Administración del sitio");
-  });
+  
+//luis
+  router.get("/cerrar-sesion", authController.cerrarSesion);
 
   // Rutas para productos
   router.get(
@@ -85,14 +62,156 @@ module.exports = () => {
     productoController.crearProducto
   );
 
+  router.get("/perfil", authController.verificarInicioSesion, usuarioController.perfil);
+  
+  
 
   router.get("/producto/:url", productoController.verProducto);
-/*
-  router.get(
-    "/carrito/:url",
-    authController.verificarInicioSesion,
-    productoController.agregarProductoCarrito
+
+  router.get("/carrito/:id", function(req, res, next){
+    var productoId = req.params.id;
+    var carrito = new Carrito(req.session.carrito ? req.session.carrito : {} );
+  
+    Producto.findById(productoId, function(err, producto){
+     if (err){
+        return res.redirect("/");
+      }
+      carrito.add(producto, producto.id);
+      req.session.carrito = carrito;
+      console.log(req.session.carrito);
+      res.redirect("/");
+    });
+    
+});
+
+router.get("/carrito", function(req, res, next) {
+  if (!req.session.carrito){
+    return res.render("carrito", {producto: null});
+  }
+  var carrito = new Carrito(req.session.carrito);
+  res.render("carrito", {producto: carrito.generarArray(), precioTotal: carrito.precioTotal});
+});
+
+router.get("/gracias", function(req, res, next) {
+  var carrito = new Carrito(req.session.carrito);
+  var orden = new Orden({
+    user: req.user,
+    name: req.user.nombre,
+    email: req.user.email,
+    cart: carrito
+  });
+  orden.save(function(err, result){
+    req.session.carrito = null;
+    return res.render("gracias");
+  });
+  
+});
+
+/*router.get("/misCompras",  function(req, res, next) {
+  Orden.find({user: req.user}, function(err, ordenes) {
+    if (err) {
+       return res.write("Error!"); 
+     }
+     var carrito;
+     ordenes.forEach(function(orden){
+      carrito =  new Carrito(orden.carrito);
+      
+       orden.items = carrito.generarArray();
+       console.log(orden.items);
+     });   
+     res.render("misCompras", {ordenes: ordenes});
+  });
+ });*/
+  
+
+ router.use("/", noLogeado, function(req, res, next){
+    next();
+  });
+ //
+
+ router.get("/crear-cuenta", usuarioController.formularioCrearCuenta);
+
+  router.post(
+    "/crear-cuenta",
+    [
+      // Realizar una verificación de los atributos del formulario
+      // https://express-validator.github.io/docs/index.html
+      check("nombre", "Debes ingresar tu nombre completo.")
+        .not()
+        .isEmpty()
+        .escape(),
+      check("email", "Debes ingresar un correo electrónico.").not().isEmpty(),
+      check("email", "El correo electrónico ingresado no es válido.")
+        .isEmail()
+        .normalizeEmail(),
+      check("password", "Debes ingresar una contraseña").not().isEmpty(),
+    ],
+    usuarioController.crearCuenta
   );
-*/
+
+  router.post("/registrarse",[
+    //Realizar una verificacion de los atributos del formulario 
+    //https://express-validator.github.io/docs/index.html
+    check("nombre", "Ingresa tu nombre completo, es requerido")
+    .not()
+    .isEmpty()
+    .escape(),
+    check("email", "Ingresa un correo electrónico")
+    .not()
+    .isEmpty(),
+    check("email", "El correo no es válido")
+    .isEmail()
+    .normalizeEmail(),
+    check("password", "Debes ingresar una contraseña")
+    .not()
+    .isEmpty()],
+    usuarioController.crearCuenta
+   );
+  
+  router.get("/iniciar-sesion", usuarioController.formularioIniciarSesion);
+
+  router.post("/iniciar-sesion", authController.autenticarUsuario);
+
+  router.get("/olvide-password", authController.formularioRestablecerPassword);
+
+  router.post("/olvide-password", authController.enviarToken);
+
+  router.get("/olvide-password/:token", authController.formularioNuevoPassword);
+
+  router.post("/olvide-password/:token", authController.almacenarNuevaPassword);
+
+  // Rutas de administración
+  router.get("/administrar", (req, res, next) => {
+    res.send("Administración del sitio");
+  });
+
+  
+
+
   return router;
 };
+  
+
+//-- Luis
+function noLogeado (req, res, next) {
+  // Si el usuario se encuentra autenticado que siga con el siguiente middleware
+  if (!req.isAuthenticated()){ 
+    return next();
+  }
+ 
+  // Si no se auntenticó, redireccionar al inicio de sesión
+  res.redirect("/");
+};
+//
+function Logeado (req, res, next) {
+  // Si el usuario se encuentra autenticado que siga con el siguiente middleware
+  if (req.isAuthenticated()){ 
+    return next();
+  }
+ 
+  // Si no se auntenticó, redireccionar al inicio de sesión
+  res.redirect("/");
+};
+
+
+
